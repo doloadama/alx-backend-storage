c293c37c56324ec40ee3b1c8c883608017aa619a
doloadama <doloadama@outlook.com>
2023-02-08 18:16:43 +0000

    NOT NULL

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- 1. In and not out
 -- Create a table users
 CREATE TABLE IF NOT EXIST users (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country ENUM ('US', 'CO', 'TN') NOT NULL,
