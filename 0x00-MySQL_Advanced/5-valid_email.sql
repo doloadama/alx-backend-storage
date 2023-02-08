7b1bafdcd2a54044754d1fa709471a871f35d379
doloadama <doloadama@outlook.com>
2023-02-08 22:47:09 +0000

    .

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -8,8 +8,10 @@ CREATE trigger valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = 0;
-    END IF;
-END $$
+IF NEW.email <> OLD.email
+THEN
+    SET NEW.valid_email = 0;
+END IF;
+END 
+$$
 DELIMITER ;
