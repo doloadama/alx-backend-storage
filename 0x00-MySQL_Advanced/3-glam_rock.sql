dd5ddee6b3ad2bf4c47bc200c9aa2f732ee04f74
doloadama <doloadama@outlook.com>
2023-02-08 19:56:01 +0000

    3.Old school band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- 3. Old school band
+-- a SQL script that lists all bands with Glam rock
+-- as their main style, ranked by their longevity
+SELECT
+    band_name
+    (YEAR(split) - YEAR(formed)) AS lifespan
+FROM
+    metal_band
+WHERE
+    main_style = 'Glam rock'
+ORDER BY
+    lifespan DESC
