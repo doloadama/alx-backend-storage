3593fb0b144685d522fb2d171f84e5e47e9d13dc
doloadama <doloadama@outlook.com>
2023-02-08 20:04:34 +0000

    script error fixed ( ; ...)

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,12 @@
 -- 3. Old school band
 -- a SQL script that lists all bands with Glam rock
 -- as their main style, ranked by their longevity
-SELECT band_name
-    COALESCE(split, 2020) - formed as lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+SELECT 
+    band_name,
+    (YEAR(split) - YEAR(formed)) AS lifespan
+FROM 
+    metal_bands
+WHERE
+      main_style = 'Glam Rock'
+ORDER BY 
+    lifespan DESC;
