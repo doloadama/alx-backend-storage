ea2419dc8fc208cb420dde0981d9c5a0c9670ea6
doloadama <doloadama@outlook.com>
2023-02-08 18:19:03 +0000

    TABLE CREATION EXISTS AND NOT EXIST ;)

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- 1. In and not out
 -- Create a table users
-CREATE TABLE IF NOT EXIST users (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
