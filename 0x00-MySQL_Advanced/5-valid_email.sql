4d638c459a53c8054954f437b0ef8c457d7f8667
doloadama <doloadama@outlook.com>
2023-02-08 21:35:24 +0000

    script error fixed

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -8,7 +8,7 @@ CREATE trigger valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email != NEW.email THEN
+    IF NEW.email <> OLD.email THEN
         SET NEW.valid_email = 0;
     END IF;
 END $$
