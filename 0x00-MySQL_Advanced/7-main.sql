0f3a3ff7e2c0431828c262a759536238a5071313
doloadama <doloadama@outlook.com>
2023-02-09 20:36:30 +0000

    tests Files deleted

--- 0x00-MySQL_Advanced/7-main.sql
+++
@@ -1,9 +0,0 @@
--- Show and compute average score
-SELECT * FROM users;
-SELECT * FROM corrections;
-
-SELECT "--";
-CALL ComputeAverageScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
-
-SELECT "--";
-SELECT * FROM users;
