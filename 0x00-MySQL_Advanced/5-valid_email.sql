ccddb16de1460adb31edf26f311e3c8cf75a34f7
doloadama <doloadama@outlook.com>
2023-02-09 07:05:12 +0000

    script error fixed

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -3,15 +3,26 @@
 -- trigger that resets the attribute
 -- valid_email only when the email
 -- has been changed.
+DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
-CREATE trigger email_trigger
-BEFORE UPDATE ON users
-FOR EACH ROW
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
-IF NEW.email <> OLD.email
-THEN
-    SET NEW.valid_email = 0;
-END IF;
-END 
-$$
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
+
+    SELECT COUNT(id)
+        INTO project_count
+        FROM projects
+        WHERE name = project_name;
+    IF project_count = 0 THEN
+        INSERT INTO projects(name)
+            VALUES(project_name);
+    END IF;
+    SELECT id
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
+    INSERT INTO corrections(user_id, project_id, score)
+        VALUES (user_id, project_id, score);
+END $$
 DELIMITER ;
