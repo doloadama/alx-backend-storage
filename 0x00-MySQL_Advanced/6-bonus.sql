3e32821f7ea39c2a2ffbeda3114047a8dbf2eb2c
doloadama <doloadama@outlook.com>
2023-02-09 07:14:22 +0000

    A little error (:

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -10,7 +10,7 @@ BEGIN
     DECLARE project_id INT;
     IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0 
     THEN
-        INSERT INTO projectS (name) VALUES (project_name);
+        INSERT INTO projects (name) VALUES (project_name);
     END IF;
     SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
     INSERT INTO corrections (user_id, project_id, score) VALUES (project_id, user_id, score);
