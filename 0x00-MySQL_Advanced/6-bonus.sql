645376092ea52731c86344c83e110b4506b36015
doloadama <doloadama@outlook.com>
2023-02-09 07:17:17 +0000

    Pattern error fixed

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -13,7 +13,7 @@ BEGIN
         INSERT INTO projects (name) VALUES (project_name);
     END IF;
     SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
-    INSERT INTO corrections (user_id, project_id, score) VALUES (project_id, user_id, score);
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id ,score);
 END;
 $$
 DELIMITER ;
