988b771e32fad046f6d9853eb55a627e744ec09d
doloadama <doloadama@outlook.com>
2023-02-14 10:22:24 +0000

    missing attributes name

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,12 +3,12 @@
 10.Change school topics
 """
 
-def update_topics(mongo_collection, topics):
+def update_topics(mongo_collection, name, topics):
     """
     function that changes all topics of a school
     document based on the name
     """
     mongo_collection.update_many(
         {"name": name},
-        {"$set": {"topics": topic}},
+        {"$set": {"topics": topics}},
     )
