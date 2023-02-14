1eb6c021d4fe2c1aea84e3084851a18c6c298cf0
doloadama <doloadama@outlook.com>
2023-02-14 10:19:42 +0000

    changes all the topics of a school document

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+10.Change school topics
+"""
+
+def update_topics(mongo_collection, topics):
+    """
+    function that changes all topics of a school
+    document based on the name
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topic}},
+    )
