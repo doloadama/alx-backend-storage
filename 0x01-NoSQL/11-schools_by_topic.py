6553bf6083378aa6db8799ece349a8c7c28f2721
doloadama <doloadama@outlook.com>
2023-02-14 10:29:07 +0000

    returns the list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+11.Where can I learn Python
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+     function that returns the list of school
+    having a specific topic:
+    """
+
+    return mongo_collection.find({'topic': topic})
