f776a94d71c41622431c1b288505db6896acd562
doloadama <doloadama@outlook.com>
2023-02-14 11:28:08 +0000

    .

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python3
-'''12. Log stats.
-'''
+"""
+Log stats
+"""
 from pymongo import MongoClient
 
 
-client = MongoClient()
-db = client.logs
-nginx = db.nginx
+def log_stats():
+    """ log_stats.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{total} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
+    print("IPs:")
+    sorted_ips = logs_collection.aggregate(
+        [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+         {"$sort": {"count": -1}}])
+    i = 0
+    for s in sorted_ips:
+        if i == 10:
+            break
+        print(f"\t{s.get('_id')}: {s.get('count')}")
+        i += 1
 
-# Get total number of logs
-num_logs = nginx.count_documents({})
 
-# Get number of logs for each HTTP method
-num_get = nginx.count_documents({"method": "GET"})
-num_post = nginx.count_documents({"method": "POST"})
-num_put = nginx.count_documents({"method": "PUT"})
-num_patch = nginx.count_documents({"method": "PATCH"})
-num_delete = nginx.count_documents({"method": "DELETE"})
-
-# Get number of logs with method=GET and path=/status
-num_status_check = nginx.count_documents({"method": "GET", "path": "/status"})
-
-# Print results
-print(f"{num_logs} logs")
-print("Methods:")
-print(f"\tmethod GET: {num_get}")
-print(f"\tmethod POST: {num_post}")
-print(f"\tmethod PUT: {num_put}")
-print(f"\tmethod PATCH: {num_patch}")
-print(f"\tmethod DELETE: {num_delete}")
-print(f"{num_status_check} status check")
+if __name__ == "__main__":
+    log_stats()
