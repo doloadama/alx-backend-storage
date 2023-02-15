41598b3cc1446810a2402e05b09e849f297564f4
doloadama <doloadama@outlook.com>
2023-02-15 10:22:39 +0000

    main file

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-import redis
+""" Main file """
 
 Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
+inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
